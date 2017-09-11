9dc9f7bd5c41ab03ac04b4062dcb1b02bdb0206c
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-11 03:57:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function currentLine(line) {
       array.push(`The line is currently empty.`);
       return array;
     } else if (line.length >= 1) {
-      array.push(`The line is currently: ${line[i]}`)
+      array.push(`The line is currently: ${line[l]}`)
       return array;
     }
 
